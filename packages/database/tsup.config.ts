dd1441491dd514bf4f9bfe90af3dbb33d8daa84f
H.W <olt13mm@gmail.com>
2025-11-23 18:02:57 +0900

    chore: remove unused tsup configuration file

--- packages/database/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup';
-
-export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['cjs', 'esm'],
-  dts: false, // DTS 생성 비활성화
-  sourcemap: true,
-  clean: true,
-});
